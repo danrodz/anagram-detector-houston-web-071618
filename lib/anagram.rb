45bcd1e824ebb31338e7db1fc27cf22b6e66490c
danrodz <daniel.a.rodriguez@dr.com>
2018-07-21 18:04:35 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+   @word = word.split('').sort!
+  end
+
+  def match(arr)
+    matches = []
+    arr.each do |elem|
+      elem_arr = elem.split('').sort!
+      matches << elem if elem_arr.eql?(@word)
+    end
+    matches
+  end
+
+end
